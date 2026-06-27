286ba93098cae9795aef848c9debe5b5bc027b8b
Denis Kolesnichenko <dentv2017@gmail.com>
2026-06-27 16:08:32 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from app.core.database import engine, Base
+from app.weather.router import router as weather_router
+from app.weather.models import WeatherHistory
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.include_router(weather_router, prefix="/api/v1")
 
 @app.get("/")
 def root():
